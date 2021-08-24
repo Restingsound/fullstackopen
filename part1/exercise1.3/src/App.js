ca2eba8acae2ecfec5f948bd985a03d70258073b
Scott Mulderig <scott.mulderig@focus-works.com>
2021-08-23 22:48:21 -0400

    part 1.3 commit

---
+++ part1/exercise1.3/src/App.js
@@ -0,0 +1,64 @@
+import React from "react";
+
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  );
+};
+
+const Part = (props) => {
+  return (
+    <div>
+      <p>
+        {props.part.name} {props.part.exercises}
+      </p>
+    </div>
+  );
+};
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.part1} />
+      <Part part={props.part2} />
+      <Part part={props.part3} />
+    </div>
+  );
+};
+
+const Total = (props) => {
+  const exerciseCount = props.exercises;
+  return (
+    <div>
+      <p>Number of exercises {exerciseCount}</p>
+    </div>
+  );
+};
+
+const App = () => {
+  const course = "Half Stack application development";
+  const part1 = {
+    name: "Fundamentals of React",
+    exercises: 10,
+  };
+  const part2 = {
+    name: "Using props to pass data",
+    exercises: 7,
+  };
+  const part3 = {
+    name: "State of a component",
+    exercises: 14,
+  };
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content part1={part1} part2={part2} part3={part3} />
+      <Total exercises={part1.exercises + part2.exercises + part3.exercises} />
+    </div>
+  );
+};
+
+export default App;
