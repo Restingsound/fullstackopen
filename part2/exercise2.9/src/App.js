f34662d51073f662fd242a80912c0dad72b96e43
Scott Mulderig <scott.mulderig@focus-works.com>
2021-08-25 22:31:00 -0400

    exercises 2.6-2.10

---
+++ part2/exercise2.9/src/App.js
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456" },
+    { name: "Ada Lovelace", number: "39-44-5323523" },
+    { name: "Dan Abramov", number: "12-43-234345" },
+    { name: "Mary Poppendieck", number: "39-23-6423122" },
+  ]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+
+  const [filter, setNewFilter] = useState("");
+
+  const addPerson = (event) => {
+    event.preventDefault();
+
+    if (persons.some((element) => element.name === newName)) {
+      window.alert(`${newName} is already added to the phonebook`);
+    } else {
+      const personObject = {
+        name: newName,
+        number: newNumber,
+      };
+      setPersons(persons.concat(personObject));
+      setNewName("");
+      setNewNumber("");
+    }
+  };
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
+  };
+
+  const handleFilterChange = (event) => {
+    setNewFilter(event.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value={filter} onChange={handleFilterChange} />
+      </div>
+      <h2>add a new</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons
+        .filter((person) =>
+          person.name.toUpperCase().includes(filter.toUpperCase())
+        )
+        .map((person) => (
+          <div key={person.name}>
+            {person.name} {person.number}
+          </div>
+        ))}
+    </div>
+  );
+};
+
+export default App;
